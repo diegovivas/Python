04b25871e2cc4ebb51595d21c0393b2cf2a62f0f
diego vivas <dieguitovivas@hotmail.com>
2020-02-11 20:33:08 -0500

    [feature] adding response of body and title of news

--- web_scrapper_curso/news_page_objects.py
+++ web_scrapper_curso/news_page_objects.py
@@ -3,8 +3,7 @@ import bs4
 
 from common import config
 
-class HomePage:
-
+class NewsPage:
     def __init__(self, news_site_uid, url):
         self._config = config()['news_sites'][news_site_uid]
         self._queries = self._config['queries']
@@ -12,8 +11,22 @@ class HomePage:
 
         self._visit(url)
 
+
     def _select(self, query_string):
         return self._html.select(query_string)
+
+
+    def _visit(self, url):
+        response = requests.get(url)
+        response.raise_for_status()
+        self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+
+        
+class HomePage(NewsPage):
+
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
+        
 
     @property
     def article_links(self):
@@ -23,8 +36,16 @@ class HomePage:
                 link_list.append(link)
 
         return set(link['href'] for link in link_list)
-        
-    def _visit(self, url):
-        response = requests.get(url)
-        response.raise_for_status()
-        self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+
+class ArticlePage(NewsPage):
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
+
+    @property
+    def body(self):
+        result = self._select(self._queries['article_body'])
+        return result[0].text if len(result) else ''
+    @property
+    def title(self):        
+        result = self._select(self._queries['article_title'])
+        return result[0].text if len(result) else ''
